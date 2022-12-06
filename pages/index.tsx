e9f83da25cd37dc3b22358bcf142ef556529c52d
PierraMuhammad <91307555+PierraMuhammad@users.noreply.github.com>
2022-12-06 23:33:01 +0700

    update html page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Sign up <a href="/signin_artist">Here</a>
         </h1>
 
         <p className={styles.description}>
